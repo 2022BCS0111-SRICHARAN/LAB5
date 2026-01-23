573e4d05001576365eaeea43637868500ea582f0
sricharanboppudi1271012-crypto <sricharanboppudi1271012@gmail.com>
2026-01-23 17:04:50 +0530

    Restore repo state and save Lab 3 progress

---
+++ train.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import json
+import os
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Lasso
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Create output directory
+os.makedirs("output", exist_ok=True)
+
+# Load dataset
+data = pd.read_csv("dataset/winequality-red.csv", sep=';')
+
+# Features and target
+X = data.drop("quality", axis=1)
+y = data["quality"]
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+
+# Scaling
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Model 
+model = Lasso(alpha=0.1)
+
+# Train model
+model.fit(X_train, y_train)
+
+# Predictions
+y_pred = model.predict(X_test)
+
+# Metrics
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+# Print metrics (required)
+print(f"MSE: {mse}")
+print(f"R2 Score: {r2}")
+
+# Save model
+joblib.dump(model, "output/model.pkl")
+
+# Save results
+results = {
+    "MSE": mse,
+    "R2": r2
+}
+
+with open("output/results.json", "w") as f:
+    json.dump(results, f, indent=4)
